b817d3f80b6887663fadd7581264ae448f942e85
anweshadas0312 <anwesharimi123@gmail.com>
2024-04-09 14:41:40 +0530

    first commit

---
+++ src/Components/Assets/data.js
@@ -0,0 +1,34 @@
+import p1_img from './product_1.png'
+import p2_img from './product_2.png'
+import p3_img from './product_3.png'
+import p4_img from './product_4.png'
+
+let data_product = [
+  {
+    id:1,
+    name:"QUECY Women's Wool Blend Hooded Long Sleeves Striped Jacket | Stylish Jackets for Women",
+    image:p1_img,
+    new_price:50.00,
+    old_price:80.50,
+  },
+  {id:2,
+    name:"VIMAL JONNEY Round Neck Cotton Solid TShirt for Women VIMAL 53224",
+    image:p2_img,
+    new_price:85.00,
+    old_price:120.50,
+  },
+  {id:3,
+    name:"Sleeveless Overlap Printed Peplum Hem Crop Top",
+    image:p3_img,
+    new_price:60.00,
+    old_price:100.50,
+  },
+  {id:4,
+    name:"Full Sleeve Overlap Coller Peplum Hem Top",
+    image:p4_img,
+    new_price:100.00,
+    old_price:150.00,
+  },
+];
+
+export default data_product;
---
+++ src/Components/Assets/new_collections.js
@@ -0,0 +1,69 @@
+import p1_img from "./product_12.png";
+import p2_img from "./product_35.png";
+import p3_img from "./product_14.png";
+import p4_img from "./product_8.png";
+import p5_img from "./product_15.png";
+import p6_img from "./product_2.png";
+import p7_img from "./product_17.png";
+import p8_img from "./product_28.png";
+
+let new_collections = [
+  {
+    id: 12,
+    name: "Full Sleeve Deep Blue Overlap Collar Peplum Top",
+    image: p1_img,
+    new_price: 50.0,
+    old_price: 80.5,
+  },
+  {
+    id: 35,
+    name: "Boys Black Colourblocked Hooded Sweatshirt",
+    image: p2_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 14,
+    name: "Men White and Blue Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    image: p3_img,
+    new_price: 60.0,
+    old_price: 100.5,
+  },
+  {
+    id: 8,
+    name: "Half Sleeve Black Printed Overlap Collar Peplum Hem Top",
+    image: p4_img,
+    new_price: 100.0,
+    old_price: 150.0,
+  },
+  {
+    id: 15,
+    name: "Men Black Printed Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    image: p5_img,
+    new_price: 50.0,
+    old_price: 80.5,
+  },
+  {
+    id: 2,
+    name: "VIMAL JONNEY Round Neck Cotton Solid TShirt for Women VIMAL53224",
+    image: p6_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 17,
+    name: "Men Denim Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+    image: p7_img,
+    new_price: 60.0,
+    old_price: 100.5,
+  },
+  {
+    id: 28,
+    name: "Boys Green Colourblocked Sweatshirt",
+    image: p8_img,
+    new_price: 100.0,
+    old_price: 150.0,
+  },
+];
+
+export default new_collections;
